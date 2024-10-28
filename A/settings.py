946e98c265626d2720f4023119b5b371599fa895
STRstark <rezafathisamani1383@gmial.com>
2024-10-28 15:37:54 +0330

    mybe final commit

--- A/settings.py
+++ A/settings.py
@@ -26,22 +26,19 @@ DEBUG = False
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 CORS_ALLOW_METHODS = [
-    'GET',
-    'POST',
-    'PUT',
-    'PATCH',
-    'DELETE',
-    'OPTIONS',
+  "DELETE",
+  "GET",
+  "OPTIONS",
+  "PATCH",
+  "POST",
+  "PUT",
 ]
 
-CORS_ALLOW_HEADERS = [
-            'content-type',
-            'authorization',
-            ...
-        ]
 
-ALLOWED_HOSTS = ['tetris-back.chbk.app']
 
 # Application definition
 
